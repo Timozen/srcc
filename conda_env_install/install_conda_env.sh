6666f095c34e5a07f75aefe444a83bbd74332fb7
Niklas <niklas.penzel@aol.de>
2019-05-11 16:28:01 +0200

    install scripts for conda environment scripts

---
+++ conda_env_install/install_conda_env.sh
@@ -0,0 +1,8 @@
+conda env remove -n srcc
+conda create -n srcc -y python=3.7
+conda install -n srcc -y numpy
+conda install -n srcc -y scipy
+conda install -n srcc -y pandas
+conda install -n srcc -y matplotlib
+conda install -n srcc -y tensorflow-gpu
+conda install -n srcc -y keras
