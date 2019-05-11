f93b2bd3a72eb8d21740e1a7e199e206c30cf168
Niklas Penzel <niklas.penzel@uni-jena.de>
2019-05-11 16:40:39 +0200

    fixed .bat script

--- conda_env_install/install_conda_env.bat
+++ conda_env_install/install_conda_env.bat
@@ -1,8 +1,8 @@
-conda env remove -n srcc
-conda env create -n srcc python=3.7
-conda install -n srcc -y numpy
-conda install -n srcc -y scipy
-conda install -n srcc -y pandas
-conda install -n srcc -y matplotlib
-conda install -n srcc -y tensorflow-gpu
-conda install -n srcc -y keras
+call conda env remove -n srcc
+call conda env create -n -y srcc python=3.7
+call conda install -n srcc -y numpy
+call conda install -n srcc -y scipy
+call conda install -n srcc -y pandas
+call conda install -n srcc -y matplotlib
+call conda install -n srcc -y tensorflow-gpu
+call conda install -n srcc -y keras
