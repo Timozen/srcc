022fe510e87e00524167abb3fa07808528d75eac
Tim Buechner <tim.buechner04@gmail.com>
2019-06-09 16:31:09 +0200

    Implementing basic dense net functions

---
+++ neural networks/srdense/srdense.py
@@ -0,0 +1,137 @@
+from contextlib import redirect_stdout
+
+from memory_usage import get_model_memory_usage
+
+from keras.utils.vis_utils import plot_model
+from keras.regularizers import l2
+# imports for dense layer
+from keras.layers import Conv2D, BatchNormalization, ReLU
+# imports for dense block
+from keras.layers import concatenate
+# imports for transition block
+from keras.layers import AveragePooling2D
+
+
+# for the model
+from keras.layers import Input, Flatten, Dense
+from keras.models import Model
+from keras.optimizers import SGD
+from keras.losses import categorical_crossentropy
+
+# testing stuff
+from keras.datasets import mnist
+from keras.utils import to_categorical
+
+
+def dense_layer(inputs, filters=12, kernel_size=(3, 3), strides=(1, 1)):
+    """
+    This function will build a single dense layer of a dense block.
+
+    inputs -- the outputs from the prev. layer
+    filters -- how many conv. layers will be created
+    kernel_size -- size of the conv. kernel
+    strides -- the stride size of the kernel, should be (1,1) in most cases
+    """
+    x = BatchNormalization()(inputs)
+    x = ReLU()(x)
+    x = Conv2D(filters=filters,
+               strides=strides,
+               kernel_size=kernel_size,
+               padding='same',
+               kernel_initializer='he_normal',
+               use_bias=False)(x)
+    return x
+
+
+def dense_block(x, layers=3, filters=12, kernel_size=(3, 3), strides=(1, 1)):
+    """
+    This function will bild one dense block with k dense layers.
+
+    inputs -- the outputs from the prev. layer
+    layers -- how many dense layers should be in one dense block
+    filters -- how many conv. layers will be created
+    kernel_size -- size of the conv. kernel
+    strides -- the stride size of the kernel, should be (1,1) in most cases
+    """
+
+    # add the k layers
+    for _ in range(layers):
+        # create one dense layers
+        cb = dense_layer(x)
+        # connected the output from all prev. ones
+        x = concatenate([x, cb], axis=3)
+
+    return x
+
+
+def transition_layer(inputs, filters=12, compression=1.0, weight_decay=1e-4):
+    """
+    This layer is between two dense blocks.
+    """
+    x = BatchNormalization()(inputs)
+    x = ReLU()(x)
+    x = Conv2D(filters=int(filters * compression),
+               kernel_size=(1, 1),
+               padding="same",
+               use_bias=False,
+               kernel_initializer='he_normal',
+               kernel_regularizer=l2(weight_decay))(x)
+    #x = AveragePooling2D((2, 2), strides=(2, 2))(x)
+    return x
+
+
+def dense_model(x_train, blocks=[3, 4, 5], filters=12, kernel_size=(3, 3), strides=(1, 1), weight_decay=1e-4):
+    if len(blocks) < 2:
+        return None
+
+    inputs = Input(x_train.shape[1:])
+
+    x = Conv2D(filters=filters,
+               kernel_size=kernel_size,
+               kernel_initializer='he_normal', 
+               padding='same',
+               strides=strides, 
+               use_bias=False, 
+               kernel_regularizer=l2(weight_decay))(inputs)
+
+    # create the dense blocks, but the last one
+    # doesnt need a transition layer
+    for i in range(len(blocks) - 1):
+        x = dense_block(x, layers=blocks[i], filters=filters)
+        x = transition_layer(x)
+    x = dense_block(x, layers=blocks[-1], filters=filters)
+
+    x = Flatten()(x)
+    x = Dense(200, activation='softmax')(x)
+    predictions = Dense(10, activation='softmax')(x)
+
+    model = Model(input=inputs, output=predictions)
+
+    model.compile(loss=categorical_crossentropy,
+                  optimizer=SGD(lr=1e-2, decay=1e-6),
+                  metrics=['accuracy'])
+    return model
+
+
+def main():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train = x_train.reshape((60000, 28, 28, 1))[:5000]
+    y_train = to_categorical(y_train, 10)[:5000]
+
+    dense_model_net = dense_model(x_train, blocks=[3,4,5], filters=12)
+    dense_model_net.summary()
+
+    with open('srdense_summary.txt', 'w') as f:
+        with redirect_stdout(f):
+            dense_model_net.summary()
+
+    print(f"Memory usage of model {get_model_memory_usage(32, dense_model_net)}")
+    plot_model(dense_model_net, 'plot_srdense.pdf')
+    plot_model(dense_model_net, 'plot_srdense_shapes.pdf', show_shapes=True)
+
+    #dense_model_net.fit(x_train, y_train, epochs = 80, shuffle= True, validation_split=0.1)
+
+
+if __name__ == "__main__":
+    main()
