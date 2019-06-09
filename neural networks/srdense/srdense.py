fa427d46f098d9d9c63eb29cdd3ea3893ad8ceed
Tim Buechner <tim.buechner04@gmail.com>
2019-06-09 19:05:14 +0200

    Added the growth rate to the model

--- neural networks/srdense/srdense.py
+++ neural networks/srdense/srdense.py
@@ -43,7 +43,7 @@ def dense_layer(inputs, filters=12, kernel_size=(3, 3), strides=(1, 1)):
     return x
 
 
-def dense_block(x, layers=3, filters=12, kernel_size=(3, 3), strides=(1, 1)):
+def dense_block(x, layers=3, filters=16, grow_filters=True, growth_rate=16, kernel_size=(3, 3), strides=(1, 1)):
     """
     This function will bild one dense block with k dense layers.
 
@@ -55,16 +55,21 @@ def dense_block(x, layers=3, filters=12, kernel_size=(3, 3), strides=(1, 1)):
     """
 
     # add the k layers
-    for _ in range(layers):
+    for i in range(layers):
         # create one dense layers
-        cb = dense_layer(x)
+        cb = dense_layer(x, filters=growth_rate)
         # connected the output from all prev. ones
-        x = concatenate([x, cb], axis=3)
+        if i == 0:
+            x = cb
+        else:
+            x = concatenate([x, cb], axis=3)
+        if growth_rate:
+            filters += growth_rate
 
-    return x
+    return x, filters
 
 
-def transition_layer(inputs, filters=12, compression=1.0, weight_decay=1e-4):
+def transition_layer(inputs, filters=16, compression=1.0, weight_decay=1e-4):
     """
     This layer is between two dense blocks.
     """
@@ -80,7 +85,7 @@ def transition_layer(inputs, filters=12, compression=1.0, weight_decay=1e-4):
     return x
 
 
-def dense_model(x_train, blocks=[3, 4, 5], filters=12, kernel_size=(3, 3), strides=(1, 1), weight_decay=1e-4):
+def dense_model(x_train, blocks=[3, 4, 5], filters=16, grow_filters=True, growth_rate=16, kernel_size=(3, 3), strides=(1, 1), weight_decay=1e-4):
     if len(blocks) < 2:
         return None
 
@@ -88,18 +93,18 @@ def dense_model(x_train, blocks=[3, 4, 5], filters=12, kernel_size=(3, 3), strid
 
     x = Conv2D(filters=filters,
                kernel_size=kernel_size,
-               kernel_initializer='he_normal', 
+               kernel_initializer='he_normal',
                padding='same',
-               strides=strides, 
-               use_bias=False, 
+               strides=strides,
+               use_bias=False,
                kernel_regularizer=l2(weight_decay))(inputs)
 
     # create the dense blocks, but the last one
     # doesnt need a transition layer
     for i in range(len(blocks) - 1):
-        x = dense_block(x, layers=blocks[i], filters=filters)
-        x = transition_layer(x)
-    x = dense_block(x, layers=blocks[-1], filters=filters)
+        x, filters = dense_block(x, layers=blocks[i], filters=filters, grow_filters=grow_filters, growth_rate=growth_rate)
+        x = transition_layer(x, filters=filters)
+    x, filters = dense_block(x, layers=blocks[-1], filters=filters, grow_filters=grow_filters, growth_rate=growth_rate)
 
     x = Flatten()(x)
     x = Dense(200, activation='softmax')(x)
@@ -119,7 +124,7 @@ def main():
     x_train = x_train.reshape((60000, 28, 28, 1))[:5000]
     y_train = to_categorical(y_train, 10)[:5000]
 
-    dense_model_net = dense_model(x_train, blocks=[3,4,5], filters=12)
+    dense_model_net = dense_model(x_train, blocks=[8,8,8,8], filters=16, growth_rate=16)
     dense_model_net.summary()
 
     with open('srdense_summary.txt', 'w') as f:
@@ -130,7 +135,7 @@ def main():
     plot_model(dense_model_net, 'plot_srdense.pdf')
     plot_model(dense_model_net, 'plot_srdense_shapes.pdf', show_shapes=True)
 
-    #dense_model_net.fit(x_train, y_train, epochs = 80, shuffle= True, validation_split=0.1)
+    dense_model_net.fit(x_train, y_train, epochs=80, shuffle=True, validation_split=0.1)
 
 
 if __name__ == "__main__":
