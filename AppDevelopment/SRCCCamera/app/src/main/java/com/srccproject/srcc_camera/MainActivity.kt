980a082731af97a37ccc805283ce545b101ffa99
DESKTOP-8D5K8GP\Quelli <quelli@posteo.de>
2019-06-20 23:59:39 +0200

    Tried Camera Intent instead of camera2 api

--- AppDevelopment/SRCCCamera/app/src/main/java/com/srccproject/srcc_camera/MainActivity.kt
+++ AppDevelopment/SRCCCamera/app/src/main/java/com/srccproject/srcc_camera/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.ImageFormat
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraCharacteristics
@@ -13,17 +14,25 @@ import android.hardware.camera2.CameraManager
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
+import android.provider.MediaStore
 import android.provider.Settings
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.support.v4.content.FileProvider
 import android.support.v7.app.ActionBarDrawerToggle
 import android.view.Surface
 import android.view.SurfaceHolder
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.jar.Manifest
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -100,6 +109,10 @@ class MainActivity : AppCompatActivity() {
         recreate()
     }
 
+    val CAMERA_REQUEST_CODE = 1
+
+    val REQUEST_TAKE_PHOTO = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -109,9 +122,94 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        surfaceView.holder.addCallback(surfaceReadyCallback)
+
+
+
+        imageButton.setOnClickListener{
+            dispatchTakePictureIntent()
+            galleryAddPic()
+            /*
+            val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (callCameraIntent.resolveActivity(packageManager) != null){
+                startActivityForResult(callCameraIntent, CAMERA_REQUEST_CODE)
+            }
+            */
+        }
+        //surfaceView.holder.addCallback(surfaceReadyCallback)
     }
 
+    private fun galleryAddPic() {
+        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+            val f = File(currentPhotoPath)
+            mediaScanIntent.data = Uri.fromFile(f)
+            sendBroadcast(mediaScanIntent)
+        }
+    }
+
+
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.android.fileprovider",
+                        it
+                    )
+
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                }
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode,resultCode,data)
+
+        when(requestCode) {
+            REQUEST_TAKE_PHOTO -> {
+                if (resultCode == Activity.RESULT_OK && data != null){
+                    imageView.setImageBitmap(MediaStore.EXTRA_OUTPUT as Bitmap)
+                }
+            }
+            else -> {
+                Toast.makeText(this, "Unrecognized request code!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    var currentPhotoPath : String = ""
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat ("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+
+    /*
     @SuppressLint("MissingPermission")
     private fun startCameraSession() {
 
@@ -134,13 +232,10 @@ class MainActivity : AppCompatActivity() {
 
                 cameraCharacteristics[CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP]?.let { streamConfigurationMap ->
                     streamConfigurationMap.getOutputSizes(ImageFormat.YUV_420_888)?.let { yuvSizes ->
-                        val previewSize = yuvSizes.last()
-// cont.
+                        val previewSize = yuvSizes.last() // cont.
                         val displayRotation = windowManager.defaultDisplay.rotation
 
-                        val swappedDimensions = areDimensionsSwapped(displayRotation, cameraCharacteristics)
-
-// swap width and height if needed
+                        val swappedDimensions = areDimensionsSwapped(displayRotation, cameraCharacteristics)  // swap width and height if needed
                         val rotatedPreviewWidth = if (swappedDimensions) previewSize.height else previewSize.width
                         val rotatedPreviewHeight = if (swappedDimensions) previewSize.width else previewSize.height
 
@@ -206,4 +301,5 @@ class MainActivity : AppCompatActivity() {
         }
         return swappedDimensions
     }
+    */
 }
