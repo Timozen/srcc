1c7fb82c87c3a6e91a51a63c9c1e835deb226465
Tim Buechner <tim.buechner04@gmail.com>
2019-06-22 22:05:10 +0200

    added some smoothing for the tile borders

--- neural networks/srdense/compare_img.py
+++ neural networks/srdense/compare_img.py
@@ -37,8 +37,51 @@ def stitch_images(images, width, height, x_dim, y_dim):
     return stitched_image
 
 
+def remove_raster(image, width, height, x_dim, y_dim):
+    total_width = width * x_dim
+    total_height = height * y_dim
+    mask = np.zeros((total_height, total_width), dtype=np.uint8)
+
+    ret = np.copy(image)
+
+    # TODO this can be done smarter...
+    # mark the tile borders as 1
+    x = width
+    while x < total_width:
+        mask[:, (x-2):(x+1)] = 1
+        x += width
+
+    y = height
+    while y < total_height:
+        mask[(y-2):(y+1), :] = 1
+        y += height
+
+    # get all indexes
+    idy, idx = np.nonzero(mask)
+
+    # average all these points with the neighbours
+    for x, y in zip(idx, idy):
+        x_min = x-1 if x-1 >=0 else 0
+        x_max = x+2 if x+2 <= total_width else total_width   
+        y_min = y-1 if y-1 >=0 else 0
+        y_max = y+2 if y+2 <=total_height else total_height
+
+        if y_min == y_max:
+            y_min -=1
+        if x_min == x_max:
+            x_min -=1
+
+        # each color channel
+        r = np.sum(image[y_min:y_max, x_min:x_max, 0]) / ((x_max-x_min) * (y_max - y_min)) 
+        g = np.sum(image[y_min:y_max, x_min:x_max, 1]) / ((x_max-x_min) * (y_max - y_min)) 
+        b = np.sum(image[y_min:y_max, x_min:x_max, 2]) / ((x_max-x_min) * (y_max - y_min))
+
+        ret[y, x] = [r, g, b]     
+
+    return ret.astype(np.uint8)
+
+
 def main():
-    path_to_file = "tim_animal_0001_0072.jpg"
     path_lr = "..\\..\\DSIDS\\LR\\tiles\\4x_cubic\\ignore"
     path_hr = "..\\..\\DSIDS\\HR\\tiles\\ignore"
     path_sr = "pred"
@@ -46,33 +89,34 @@ def main():
     img_lr = stitch_images(load_images("tim_animal_0001_", path_lr), 42, 42, 23, 17)
     img_hr = stitch_images(load_images("tim_animal_0001_", path_hr), 42*4, 42*4, 23, 17)
     img_sr = stitch_images(load_images("pred_", path_sr), 42*4, 42*4, 23, 17)
+
+    img_sr_cleaned = remove_raster(img_sr,  42*4, 42*4, 23, 17)
+
     fig = plt.figure("Compare", figsize=(16, 8))
 
-    #ax1 = fig.add_subplot(1, 3, 1)
-    ax2 = fig.add_subplot(1, 2, 1)
-    ax3 = fig.add_subplot(1, 2, 2)
-    #ax4 = fig.add_subplot(1, 4, 4)
+    ax1 = fig.add_subplot(1, 3, 1)
+    ax2 = fig.add_subplot(1, 3, 2)
+    ax3 = fig.add_subplot(1, 3, 3)
 
-    # ax1.imshow(cv2.cvtColor(img_lr, cv2.COLOR_BGR2RGB))
-    # ax1.title.set_text("LR Image")
-    ax2.imshow(cv2.cvtColor(img_hr, cv2.COLOR_BGR2RGB))
-    ax2.title.set_text("HR Image")
-    ax3.imshow(cv2.cvtColor(img_sr, cv2.COLOR_BGR2RGB))
-    ax3.title.set_text("SR Image")
+    ax1.imshow(cv2.cvtColor(img_hr, cv2.COLOR_BGR2RGB))
+    ax1.title.set_text("HR Image")
+    ax2.imshow(cv2.cvtColor(img_sr, cv2.COLOR_BGR2RGB))
+    ax2.title.set_text("SR Image")
+    ax3.imshow(cv2.cvtColor(img_sr_cleaned, cv2.COLOR_BGR2RGB))
+    ax3.title.set_text("SR Image Cleaned")
 
-    img_lr_upscale = cv2.resize(img_lr, dsize=(42*4*23, 42*4*17), interpolation = cv2.INTER_CUBIC)
+    img_lr_upscale = cv2.resize(img_lr, dsize=(42*4*23, 42*4*17), interpolation=cv2.INTER_CUBIC)
 
     cv2.imwrite("img_lr.jpg", img_lr)
     cv2.imwrite("img_lr_upscale.jpg", img_lr_upscale)
     cv2.imwrite("img_hr.jpg", img_hr)
     cv2.imwrite("img_sr.jpg", img_sr)
+    cv2.imwrite("img_mask.jpg", img_sr_cleaned)
 
     # diff = np.abs(np.dot(img_sr[...,:3], [0.2989, 0.5870, 0.1140]) - np.dot(img_hr[...,:3], [0.2989, 0.5870, 0.1140]))
     # ax4.imshow(diff, cmap="gray")
     # ax4.title.set_text("Difference")
     plt.show()
-
-
 
 
 if __name__ == "__main__":
