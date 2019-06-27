4a21d12e5a2a54cd1c47677c3c99b5876c8ca144
Niklas <niklas.penzel@aol.de>
2019-06-28 00:13:25 +0200

    tiling lr image into a list of tiles

---
+++ Utils.py
@@ -0,0 +1,32 @@
+import numpy as np 
+import cv2
+from tqdm import tqdm
+
+def crop_lr_image(img, hr_shape=(336,336), overlap=False):
+    """function to crop one lr image into tiles for prediction.
+
+    img -- the lr image as a numpy array
+    shape -- the shape of the tiles
+    overlap -- should the tiles overlap by half a tile?    
+    """
+    # calc the lr tile shape
+    lr_shape = (hr_shape[0]//4, hr_shape[1]//4)
+
+    # calc the step size for the tiling according to overlap
+    if overlap:
+        # overlap -> half a tile per step
+        step_size = (lr_shape[0]//2, lr_shape[1]//2)
+    else:
+        # no overlap -> a whole tile per step
+        step_size = lr_shape
+
+    tile_list = []
+    for i in range(0, img.shape[0], step_size[0]):
+        for j in range(0, img.shape[1], step_size[1]):
+            # test if this tile is part of the image
+            if i + lr_shape[0] <= img.shape[0] and j + lr_shape[1] <= img.shape[1]:
+                # append the new tile
+                tile_list.append(img[i:i + lr_shape[0], j:j + lr_shape[1],:])
+    # return the list of tiles
+    return tile_list
+
