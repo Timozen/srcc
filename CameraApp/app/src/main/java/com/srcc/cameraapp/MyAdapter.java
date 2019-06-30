599558b57efca4b19bb0aa96f3a64c382fd028f0
Tim Buechner <tim.buechner04@gmail.com>
2019-06-30 10:45:17 +0200

    Small cleanups

--- CameraApp/app/src/main/java/com/srcc/cameraapp/MyAdapter.java
+++ CameraApp/app/src/main/java/com/srcc/cameraapp/MyAdapter.java
@@ -108,7 +108,6 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
         @Override
         public void onClick(View v) {
-            //Toast.makeText(v.getContext(), "Image Uri = " + mUri.toString(), Toast.LENGTH_SHORT).show();
 
             Intent intent = new Intent(mActivity, ViewImageActivity.class);
             intent.putExtra("Uri", mUri.toString());
--- CameraApp/app/src/main/java/com/srcc/cameraapp/ViewImageActivity.java
+++ CameraApp/app/src/main/java/com/srcc/cameraapp/ViewImageActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
