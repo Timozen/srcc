70a0f34a4f5cf0e36999d316a528f96256a3f39a
Tim Buechner <tim.buechner04@gmail.com>
2019-07-04 10:51:22 +0200

    Changed the color for the intro activity

--- CameraApp/app/src/main/java/com/srcc/cameraapp/main/IntroActivity.java
+++ CameraApp/app/src/main/java/com/srcc/cameraapp/main/IntroActivity.java
@@ -26,6 +26,8 @@ public class IntroActivity extends AppIntro {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        int color = getColor(R.color.orange_intro);
+
         Log.i(TAG, "Start Intro Activity");
 
         //Initial welcome page
@@ -33,35 +35,35 @@ public class IntroActivity extends AppIntro {
         welcomePage.setTitle("Welcome");
         welcomePage.setDescription("This app allows you to take camera pictures and those will quality will be improved.");
         welcomePage.setImageDrawable(R.drawable.ic_mood_black_128dp);
-        welcomePage.setBgColor(getColor(R.color.orange));
+        welcomePage.setBgColor(color);
 
         //Camera Permission
         SliderPage cameraPage = new SliderPage();
         cameraPage.setTitle("Camera");
         cameraPage.setDescription("Because we'd like to take pictures, we need the permission of using the camera.");
         cameraPage.setImageDrawable(R.drawable.ic_photo_camera_black_128dp);
-        cameraPage.setBgColor(getColor(R.color.orange));
+        cameraPage.setBgColor(color);
 
         //External Store Permission
         SliderPage storagePage = new SliderPage();
         storagePage.setTitle("Storage");
         storagePage.setDescription("The taken pictures and the improved pictures will be store on your phone. Therefore we need the storage permission.");
         storagePage.setImageDrawable(R.drawable.ic_storage_black_128dp);
-        storagePage.setBgColor(getColor(R.color.orange));
+        storagePage.setBgColor(color);
 
         //Internet Permission
         SliderPage internetPage = new SliderPage();
         internetPage.setTitle("Internet");
         internetPage.setDescription("To create the improved pictures the app has to communicate with our server. Therefore we need to access the internet.");
         internetPage.setImageDrawable(R.drawable.ic_internet_black_128dp);
-        internetPage.setBgColor(getColor(R.color.orange));
+        internetPage.setBgColor(color);
 
         //Usage
         SliderPage usagePage = new SliderPage();
         usagePage.setTitle("Usage");
         usagePage.setDescription("The main focus is the camera view. If you swipe to the left the settings will appear. Swiping to the right will show the taken pictures.");
         usagePage.setImageDrawable(R.drawable.ic_swipe_arrows_black_128dp);
-        usagePage.setBgColor(getColor(R.color.orange));
+        usagePage.setBgColor(color);
 
 
         addSlide(AppIntroFragment.newInstance(welcomePage));
@@ -77,7 +79,7 @@ public class IntroActivity extends AppIntro {
 
         showSkipButton(false);
         showStatusBar(false);
-        setNavBarColor(R.color.orange);
+        setNavBarColor(R.color.orange_intro);
 
     }
 
