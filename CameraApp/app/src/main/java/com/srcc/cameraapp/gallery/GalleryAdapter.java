ea497b9c4b07f7d44b062d119da80ede019b18c4
Tim Buechner <tim.buechner04@gmail.com>
2019-07-05 15:45:46 +0200

    Moved all the gallery logic from viewholder to adapter (better to understand)

--- CameraApp/app/src/main/java/com/srcc/cameraapp/gallery/GalleryAdapter.java
+++ CameraApp/app/src/main/java/com/srcc/cameraapp/gallery/GalleryAdapter.java
@@ -6,7 +6,6 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.Point;
@@ -21,22 +20,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.google.android.material.snackbar.Snackbar;
 import com.ortiz.touchview.TouchImageView;
 import com.srcc.cameraapp.R;
 import com.srcc.cameraapp.api.ApiService;
-import com.srcc.cameraapp.other.Utils;
 
-import java.io.File;
+import java.util.HashMap;
 
 import io.reactivex.disposables.CompositeDisposable;
 
@@ -49,6 +43,17 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
     private int rowCount;
     private int width;
 
+    private int shortAnimationDuration;
+    private int currentPosition;
+    private Animator currentAnimator;
+
+    private HashMap<Integer, ViewHolder> viewHolderList;
+
+    private ConstraintLayout display;
+    private ConstraintLayout gallery;
+
+    private TouchImageView touchImageView;
+
     GalleryAdapter(final Activity activity, final CompositeDisposable compositeDisposable, final ApiService apiService, int rowCount) {
         this.activity = activity;
         this.mApiConnection = apiService;
@@ -58,8 +63,13 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
         DisplayMetrics dp = new DisplayMetrics();
         activity.getWindowManager().getDefaultDisplay().getMetrics(dp);
         width = dp.widthPixels / rowCount;
+        shortAnimationDuration = activity.getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-        Log.i("Gallery", "width per item " + width);
+        viewHolderList = new HashMap<>();
+
+        gallery = activity.findViewById(R.id.constraintLayout_gallery_main);
+        display = activity.findViewById(R.id.display);
+        touchImageView = activity.findViewById(R.id.expanded_image);
     }
 
     /**
@@ -76,11 +86,9 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
         //inflate the element layout to the root
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.gallery_item_view, viewGroup, false);
         ViewHolder vh = new ViewHolder(view);
-        //set the onclick listener so we can handle clicks
-//        view.setOnClickListener(vh);
-//        view.setOnLongClickListener(vh);
-        vh.getImageView().getLayoutParams().width = width;
+        //noinspection SuspiciousNameCombination
         vh.getImageView().getLayoutParams().height = width;
+        vh.getImageView().getLayoutParams().width = width;
         return vh;
     }
 
@@ -99,6 +107,24 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
         //attach the data
         viewHolder.setUri(imageUri);
         viewHolder.setTitle(imageUri.toString());
+
+        viewHolderList.put(position, viewHolder);
+        Log.i("Gallery", "put " + position);
+
+        viewHolder.getImageView().setOnClickListener(v -> {
+            currentPosition = position;
+            goToFullImage();
+        });
+
+        viewHolder.getImageView().setOnLongClickListener(v -> {
+            if(viewHolder.isSelected) {
+                viewHolder.getImageView().setColorFilter(Color.argb(0, 0, 0, 0));
+            } else {
+                viewHolder.getImageView().setColorFilter(Color.argb(50, 0, 0, 255));
+            }
+            viewHolder.isSelected = !viewHolder.isSelected;
+            return viewHolder.isSelected;
+        });
     }
 
     /**
@@ -163,135 +189,151 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
         return Uri.parse("file://" + dataString);
     }
 
+    private void goToFullImage(){
+        if(currentAnimator != null){
+            currentAnimator.cancel();
+        }
+
+        ViewHolder currentViewHolder = viewHolderList.get(currentPosition);
+
+//                Glide.with(activity).load(mUri).centerCrop().into(imageViewLarge);
+        assert currentViewHolder != null;
+        touchImageView.setImageURI(currentViewHolder.getUri());
+
+        final Rect startBounds = new Rect();
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+
+        currentViewHolder.getImageView().getGlobalVisibleRect(startBounds);
+        gallery.getGlobalVisibleRect(finalBounds, globalOffset);
+
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        float startScale;
+        if ((float) finalBounds.width() / finalBounds.height() > (float) startBounds.width() / startBounds.height()) {
+            // Extend start bounds horizontally
+            startScale = (float) startBounds.height() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+        display.setVisibility(View.VISIBLE);
+        display.setPivotX(0f);
+        display.setPivotY(0f);
+
+        AnimatorSet set = new AnimatorSet();
+        set.play(ObjectAnimator.ofFloat(display, View.X, startBounds.left, finalBounds.left))
+                .with(ObjectAnimator.ofFloat(display, View.Y, startBounds.top, finalBounds.top))
+                .with(ObjectAnimator.ofFloat(display, View.SCALE_X, startScale, 1f))
+                .with(ObjectAnimator.ofFloat(display, View.SCALE_Y, startScale, 1f));
+
+        set.setDuration(shortAnimationDuration);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                currentAnimator = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                currentAnimator = null;
+            }
+        });
+        set.start();
+        currentAnimator = set;
+
+        final float startScaleFinal = startScale;
+        touchImageView.setOnClickListener(view1 -> {
+            returnFromFullImage(startScaleFinal);
+
+        });
+    }
+
+    private void returnFromFullImage(final float startScaleFinal){
+        if (currentAnimator != null) {
+            currentAnimator.cancel();
+        }
+
+        ViewHolder currentViewHolder = viewHolderList.get(currentPosition);
+
+        final Rect startBounds = new Rect();
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+
+        currentViewHolder.getImageView().getGlobalVisibleRect(startBounds);
+        gallery.getGlobalVisibleRect(finalBounds, globalOffset);
+
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        float startScale;
+        if ((float) finalBounds.width() / finalBounds.height() > (float) startBounds.width() / startBounds.height()) {
+            // Extend start bounds horizontally
+            startScale = (float) startBounds.height() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+        // Animate the four positioning/sizing properties in parallel,
+        // back to their original values.
+        AnimatorSet set1 = new AnimatorSet();
+        set1.play(ObjectAnimator.ofFloat(display, View.X, startBounds.left))
+                .with(ObjectAnimator.ofFloat(display,View.Y, startBounds.top))
+                .with(ObjectAnimator.ofFloat(display,View.SCALE_X, startScaleFinal))
+                .with(ObjectAnimator.ofFloat(display,View.SCALE_Y, startScaleFinal));
+
+        set1.setDuration(shortAnimationDuration);
+        set1.setInterpolator(new DecelerateInterpolator());
+        set1.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                display.setVisibility(View.GONE);
+                currentAnimator = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                display.setVisibility(View.GONE);
+                currentAnimator = null;
+            }
+        });
+        set1.start();
+        currentAnimator = set1;
+    }
+
     /**
      * This class will hold the information of our elements in the recycler view
      */
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    class ViewHolder extends RecyclerView.ViewHolder{
         private ImageView imageViewThumbnail;
         private Uri mUri;
         private String title;
         private boolean isSelected;
-        private Animator currentAnimator;
 
-        @SuppressLint("ClickableViewAccessibility")
         ViewHolder(View view) {
             super(view);
             imageViewThumbnail =  view.findViewById(R.id.imageView_gallery_item_image);
             isSelected = false;
-
-            int shortAnimationDuration = activity.getResources().getInteger(android.R.integer.config_shortAnimTime);
-            currentAnimator = null;
-
-            imageViewThumbnail.setOnClickListener(v -> {
-                if(currentAnimator != null){
-                    currentAnimator.cancel();
-                }
-                ConstraintLayout display = activity.findViewById(R.id.display);
-                TouchImageView imageViewLarge = activity.findViewById(R.id.expanded_image);
-
-
-                Log.i("gallery", "image view is " + imageViewLarge.toString());
-
-//                Glide.with(activity).load(mUri).centerCrop().into(imageViewLarge);
-                imageViewLarge.setImageURI(mUri);
-
-                final Rect startBounds = new Rect();
-                final Rect finalBounds = new Rect();
-                final Point globalOffset = new Point();
-
-                imageViewThumbnail.getGlobalVisibleRect(startBounds);
-                activity.findViewById(R.id.constraintLayout_gallery_main).getGlobalVisibleRect(finalBounds, globalOffset);
-
-                startBounds.offset(-globalOffset.x, -globalOffset.y);
-                finalBounds.offset(-globalOffset.x, -globalOffset.y);
-
-                float startScale;
-                if ((float) finalBounds.width() / finalBounds.height() > (float) startBounds.width() / startBounds.height()) {
-                    // Extend start bounds horizontally
-                    startScale = (float) startBounds.height() / finalBounds.height();
-                    float startWidth = startScale * finalBounds.width();
-                    float deltaWidth = (startWidth - startBounds.width()) / 2;
-                    startBounds.left -= deltaWidth;
-                    startBounds.right += deltaWidth;
-                } else {
-                    // Extend start bounds vertically
-                    startScale = (float) startBounds.width() / finalBounds.width();
-                    float startHeight = startScale * finalBounds.height();
-                    float deltaHeight = (startHeight - startBounds.height()) / 2;
-                    startBounds.top -= deltaHeight;
-                    startBounds.bottom += deltaHeight;
-                }
-
-                display.setVisibility(View.VISIBLE);
-                display.setPivotX(0f);
-                display.setPivotY(0f);
-
-                AnimatorSet set = new AnimatorSet();
-                set.play(ObjectAnimator.ofFloat(display, View.X, startBounds.left, finalBounds.left))
-                    .with(ObjectAnimator.ofFloat(display, View.Y, startBounds.top, finalBounds.top))
-                    .with(ObjectAnimator.ofFloat(display, View.SCALE_X, startScale, 1f))
-                    .with(ObjectAnimator.ofFloat(display, View.SCALE_Y, startScale, 1f));
-
-                set.setDuration(shortAnimationDuration);
-                set.setInterpolator(new DecelerateInterpolator());
-                set.addListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        currentAnimator = null;
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation) {
-                        currentAnimator = null;
-                    }
-                });
-                set.start();
-                currentAnimator = set;
-
-                final float startScaleFinal = startScale;
-                imageViewLarge.setOnClickListener(view1 -> {
-                    if (currentAnimator != null) {
-                        currentAnimator.cancel();
-                    }
-
-                    // Animate the four positioning/sizing properties in parallel,
-                    // back to their original values.
-                    AnimatorSet set1 = new AnimatorSet();
-                    set1.play(ObjectAnimator.ofFloat(display, View.X, startBounds.left))
-                            .with(ObjectAnimator.ofFloat(display,View.Y,startBounds.top))
-                            .with(ObjectAnimator.ofFloat(display,View.SCALE_X, startScaleFinal))
-                            .with(ObjectAnimator.ofFloat(display,View.SCALE_Y, startScaleFinal));
-
-                    set1.setDuration(shortAnimationDuration);
-                    set1.setInterpolator(new DecelerateInterpolator());
-                    set1.addListener(new AnimatorListenerAdapter() {
-                        @Override
-                        public void onAnimationEnd(Animator animation) {
-                            display.setVisibility(View.GONE);
-                            currentAnimator = null;
-                        }
-
-                        @Override
-                        public void onAnimationCancel(Animator animation) {
-                            display.setVisibility(View.GONE);
-                            currentAnimator = null;
-                        }
-                    });
-                    set1.start();
-                    currentAnimator = set1;
-                });
-
-            });
-
-            imageViewThumbnail.setOnLongClickListener(v -> {
-                if(isSelected) {
-                    imageViewThumbnail.setColorFilter(Color.argb(0, 0, 0, 0));
-                } else {
-                    imageViewThumbnail.setColorFilter(Color.argb(50, 0, 0, 255));
-                }
-                isSelected = !isSelected;
-                return isSelected;
-            });
         }
 
         ImageView getImageView() {
@@ -302,16 +344,14 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
             this.mUri = mUri;
         }
 
-        /**
-         * Parse the title which should be always some path/numbers_{lr/hr}.jpg
-         *
-         * @param title mostly the filepath
-         */
         void setTitle(String title) {
             String temp = title.split("\\.")[0];
             String[] temp2 = temp.split("\\/");
             this.title = temp2[temp2.length - 1];
-//            textViewTitle.setText(this.title);
+        }
+
+        Uri getUri() {
+            return mUri;
         }
 
         /**
