967609874245ab2430ac0da2300c687cbb34ab4a
Tim Buechner <tim.buechner04@gmail.com>
2019-07-06 08:49:52 +0200

    Fix for chrashing if going to no portrait mode

--- CameraApp/app/src/main/java/com/srcc/cameraapp/main/MainActivity.java
+++ CameraApp/app/src/main/java/com/srcc/cameraapp/main/MainActivity.java
@@ -3,9 +3,11 @@ package com.srcc.cameraapp.main;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -39,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         SharedPreferences sp =  PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         if (sp.getBoolean("firstStart", true)) {
