63fc8cd99bde6e5d7716d4dead88cb538713c381
Niklas <niklas.penzel@aol.de>
2019-07-08 19:20:53 +0200

    eval update

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -58,7 +58,8 @@ def MSSIM(true, pred, L=255, k1=0.01, k2=0.03, sub=8):
     return mssim/(sub**2)
 
 def main():
-    true = cv2.imread(os.path.join("..", "DSIDS", "HR", "niklas_animal_0010.jpg"))
+    #true = cv2.imread(os.path.join("..", "DSIDS", "HR", "niklas_animal_0010.jpg"))
+    true = cv2.cvtColor(cv2.imread(os.path.join("..", "DSIDS", "test", "20190702_210258.jpg")), cv2.COLOR_BGR2RGB)
 
     lr = cv2.resize(true, (0, 0),
                     fx=1/4,
@@ -68,7 +69,7 @@ def main():
     pred_nearest = cv2.resize(lr, (0, 0),
                       fx=4,
                       fy=4,
-                      interpolation=cv2.INTER_NEAREST)
+                      interpolation=cv2.INTER_CUBIC)
 
 
     model = load_model(os.path.join("..", "models", "initialized_gen_model20.h5"), custom_objects={"tf":tf})
@@ -103,8 +104,8 @@ def main():
     print("SSIM: ", SSIM(true,true))
     print("MSSIM: ", MSSIM(true,true))
 
-    cv2.imwrite("nearest.jpg", pred_nearest)
-    cv2.imwrite("sr.jpg", sr)
+    cv2.imwrite("nearest.jpg", cv2.cvtColor(pred_nearest, cv2.COLOR_RGB2BGR))
+    cv2.imwrite("sr.jpg", cv2.cvtColor(sr, cv2.COLOR_RGB2BGR))
 
 if __name__ == "__main__":
     main()
