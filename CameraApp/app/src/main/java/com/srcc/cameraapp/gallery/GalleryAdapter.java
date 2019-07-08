230678737ac9d2f3c0631dbce1017c5bb9ef8417
Tim Büchner <tim.buechner04@gmail.com>
2019-07-08 18:16:35 +0200

    Fix querying only the correct folder

--- CameraApp/app/src/main/java/com/srcc/cameraapp/gallery/GalleryAdapter.java
+++ CameraApp/app/src/main/java/com/srcc/cameraapp/gallery/GalleryAdapter.java
@@ -278,7 +278,7 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
                 mProjection,
                 MediaStore.Images.Media.DATA + " like ? ",
-                new String[]{"%srcc%"},
+                new String[]{"%/srcc/%"},
                 MediaStore.Images.Media.DATE_ADDED + " DESC"
         );
     }
